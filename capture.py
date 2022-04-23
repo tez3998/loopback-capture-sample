82d8e32f4e161674c27f72548e22c96034acdc19
Yuta Tezuka <yuta.tzk@gmail.com>
2022-04-23 11:48:29 +0900

    Change constant to uppercase

--- capture.py
+++ capture.py
@@ -1,13 +1,13 @@
 import soundcard as sc
 import soundfile as sf
 
-output_file_name = "out.wav"    # file name.
-samplerate = 48000              # [Hz]. sampling rate.
-record_sec = 5                  # [sec]. duration recording audio.
+OUTPUT_FILE_NAME = "out.wav"    # file name.
+SAMPLE_RATE = 48000              # [Hz]. sampling rate.
+RECORD_SEC = 5                  # [sec]. duration recording audio.
 
-with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=samplerate) as mic:
+with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
     # record audio with loopback from default speaker.
-    data = mic.record(numframes=samplerate*record_sec)
+    data = mic.record(numframes=SAMPLE_RATE*RECORD_SEC)
     
     # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
-    sf.write(file=output_file_name, data=data[:, 0], samplerate=samplerate)
+    sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
